23997ae065d42a2135a8f06b037521d675d9f3d9
Jason Paulos <jasonpaulos@users.noreply.github.com>
2019-11-18 17:14:43 -0500

    Implement packet methods

--- include/packet.h
+++ include/packet.h
@@ -45,16 +45,16 @@ typedef struct cdt_packet_t {
 int cdt_packet_self_identify_create(cdt_packet_t *packet, const char *address, const char *port);
 int cdt_packet_self_identify_parse(cdt_packet_t *packet, char **address, char **port);
 
-int cdt_packet_peer_id_assign_create(cdt_packet_t *packet, int peer_id);
-int cdt_packet_peer_id_assign_parse(cdt_packet_t *packet, int *peer_id);
+int cdt_packet_peer_id_assign_create(cdt_packet_t *packet, uint32_t peer_id);
+int cdt_packet_peer_id_assign_parse(cdt_packet_t *packet, uint32_t *peer_id);
 
 void cdt_packet_peer_id_confim_create(cdt_packet_t *packet);
 
-int cdt_packet_new_peer_create(cdt_packet_t *packet, int peer_id, const char *address, const char *port);
-int cdt_packet_new_peer_parse(cdt_packet_t *packet, int *peer_id, char **address, char **port);
+int cdt_packet_new_peer_create(cdt_packet_t *packet, uint32_t peer_id, const char *address, const char *port);
+int cdt_packet_new_peer_parse(cdt_packet_t *packet, uint32_t *peer_id, char **address, char **port);
 
-int cdt_packet_existing_peer_create(cdt_packet_t *packet, int peer_id);
-int cdt_packet_existing_peer_parse(cdt_packet_t *packet, int *peer_id);
+int cdt_packet_existing_peer_create(cdt_packet_t *packet, uint32_t peer_id);
+int cdt_packet_existing_peer_parse(cdt_packet_t *packet, uint32_t *peer_id);
 
 int cdt_packet_alloc_req_create(cdt_packet_t *packet, int peer_id);
 int cdt_packet_alloc_req_parse(cdt_packet_t *packet, int *peer_id);
@@ -62,34 +62,33 @@ int cdt_packet_alloc_req_parse(cdt_packet_t *packet, int *peer_id);
 int cdt_packet_alloc_resp_create(cdt_packet_t *packet, uint64_t page);
 int cdt_packet_alloc_resp_parse(cdt_packet_t *packet, uint64_t * page);
 
-int cdt_packet_read_req_create(cdt_packet_t *packet, unsigned long page_addr);
-int cdt_packet_read_req_parse(cdt_packet_t *packet, unsigned long *page_addr);
+void cdt_packet_read_req_create(cdt_packet_t *packet, uint64_t page_addr);
+void cdt_packet_read_req_parse(cdt_packet_t *packet, uint64_t *page_addr);
 
-int cdt_packet_read_resp_create(cdt_packet_t *packet, void *page);
-int cdt_packet_read_resp_parse(cdt_packet_t *packet, void **page);
+void cdt_packet_read_resp_create(cdt_packet_t *packet, void *page);
+void cdt_packet_read_resp_parse(cdt_packet_t *packet, void **page);
 
-int cdt_packet_read_invalidate_req_create(cdt_packet_t *packet, unsigned long page_addr);
-int cdt_packet_read_invalidate_req_parse(cdt_packet_t *packet, unsigned long *page_addr);
+void cdt_packet_read_invalidate_req_create(cdt_packet_t *packet, uint64_t page_addr);
+void cdt_packet_read_invalidate_req_parse(cdt_packet_t *packet, uint64_t *page_addr);
 
 void cdt_packet_read_invalidate_resp_create(cdt_packet_t *packet);
 
-int cdt_packet_write_req_create(cdt_packet_t *packet, unsigned long page_addr);
-int cdt_packet_write_req_parse(cdt_packet_t *packet, unsigned long *page_addr);
+void cdt_packet_write_req_create(cdt_packet_t *packet, uint64_t page_addr);
+void cdt_packet_write_req_parse(cdt_packet_t *packet, uint64_t *page_addr);
 
-int cdt_packet_write_resp_create(cdt_packet_t *packet, void *page);
-int cdt_packet_write_resp_parse(cdt_packet_t *packet, void **page);
+void cdt_packet_write_resp_create(cdt_packet_t *packet, void *page);
+void cdt_packet_write_resp_parse(cdt_packet_t *packet, void **page);
 
-int cdt_packet_write_demote_req_create(cdt_packet_t *packet, unsigned long page_addr);
-int cdt_packet_write_demote_req_parse(cdt_packet_t *packet, unsigned long *page_addr);
+void cdt_packet_write_demote_req_create(cdt_packet_t *packet, uint64_t page_addr);
+void cdt_packet_write_demote_req_parse(cdt_packet_t *packet, uint64_t *page_addr);
 
-int cdt_packet_write_demote_resp_create(cdt_packet_t *packet, void *page);
-int cdt_packet_write_demote_resp_parse(cdt_packet_t *packet, void **page);
+void cdt_packet_write_demote_resp_create(cdt_packet_t *packet, void *page);
+void cdt_packet_write_demote_resp_parse(cdt_packet_t *packet, void **page);
 
-int cdt_packet_write_invalidate_req_create(cdt_packet_t *packet, unsigned long page_addr);
-int cdt_packet_write_invalidate_req_parse(cdt_packet_t *packet, unsigned long *page_addr);
+void cdt_packet_write_invalidate_req_create(cdt_packet_t *packet, uint64_t page_addr);
+void cdt_packet_write_invalidate_req_parse(cdt_packet_t *packet, uint64_t *page_addr);
 
-int cdt_packet_write_invalidate_resp_create(cdt_packet_t *packet, void *page);
-int cdt_packet_write_invalidate_resp_parse(cdt_packet_t *packet, void **page);
-
+void cdt_packet_write_invalidate_resp_create(cdt_packet_t *packet, void *page);
+void cdt_packet_write_invalidate_resp_parse(cdt_packet_t *packet, void **page);
 
 #endif
--- src/packet.c
+++ src/packet.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include "util.h"
 #include "packet.h"
 #include "util.h"
 
@@ -29,17 +30,17 @@ int cdt_packet_self_identify_parse(cdt_packet_t *packet, char **address, char **
   return 0;
 }
 
-int cdt_packet_peer_id_assign_create(cdt_packet_t *packet, int peer_id) {
+int cdt_packet_peer_id_assign_create(cdt_packet_t *packet, uint32_t peer_id) {
   packet->type = CDT_PACKET_PEER_ID_ASSIGN;
-  packet->size = sizeof(int);
-  *(int*)packet->data = htonl(peer_id);
+  packet->size = sizeof(uint32_t);
+  *(uint32_t*)packet->data = htonl(peer_id);
 
   return 0;
 }
 
-int cdt_packet_peer_id_assign_parse(cdt_packet_t *packet, int *peer_id) {
+int cdt_packet_peer_id_assign_parse(cdt_packet_t *packet, uint32_t *peer_id) {
   assert(packet->type == CDT_PACKET_PEER_ID_ASSIGN);
-  *peer_id = ntohl(*(int*)packet->data);
+  *peer_id = ntohl(*(uint32_t*)packet->data);
   return 0;
 }
 
@@ -48,45 +49,45 @@ void cdt_packet_peer_id_confim_create(cdt_packet_t *packet) {
   packet->size = 0;
 }
 
-int cdt_packet_new_peer_create(cdt_packet_t *packet, int peer_id, const char *address, const char *port) {
+int cdt_packet_new_peer_create(cdt_packet_t *packet, uint32_t peer_id, const char *address, const char *port) {
   int address_len = strlen(address) + 1; // + 1 to include null terminating character
   int port_len = strlen(port) + 1;
 
-  if (address_len + port_len + sizeof(int) > CDT_PACKET_DATA_SIZE)
+  if (address_len + port_len + sizeof(uint32_t) > CDT_PACKET_DATA_SIZE)
     return -1;
   
   packet->type = CDT_PACKET_NEW_PEER;
-  packet->size = address_len + port_len + sizeof(int);
+  packet->size = address_len + port_len + sizeof(uint32_t);
 
-  *(int*)packet->data = htonl(peer_id);
-  memmove(packet->data + sizeof(int), address, address_len);
-  memmove(packet->data + address_len + sizeof(int), port, port_len);
+  *(uint32_t*)packet->data = htonl(peer_id);
+  memmove(packet->data + sizeof(uint32_t), address, address_len);
+  memmove(packet->data + address_len + sizeof(uint32_t), port, port_len);
 
   return 0;
 }
 
-int cdt_packet_new_peer_parse(cdt_packet_t *packet, int *peer_id, char **address, char **port) {
+int cdt_packet_new_peer_parse(cdt_packet_t *packet, uint32_t *peer_id, char **address, char **port) {
   assert(packet->type == CDT_PACKET_NEW_PEER);
 
-  *peer_id = ntohl(*(int*)packet->data);
-  *address = packet->data + sizeof(int);
+  *peer_id = ntohl(*(uint32_t*)packet->data);
+  *address = packet->data + sizeof(uint32_t);
   *port = *address + strlen(*address) + 1;
 
   return 0;
 }
 
-int cdt_packet_existing_peer_create(cdt_packet_t *packet, int peer_id) {
+int cdt_packet_existing_peer_create(cdt_packet_t *packet, uint32_t peer_id) {
   packet->type = CDT_PACKET_EXISTING_PEER;
-  packet->size = sizeof(int);
-  *(int*)packet->data = htonl(peer_id);
+  packet->size = sizeof(uint32_t);
+  *(uint32_t*)packet->data = htonl(peer_id);
 
   return 0;
 }
 
-int cdt_packet_existing_peer_parse(cdt_packet_t *packet, int *peer_id) {
+int cdt_packet_existing_peer_parse(cdt_packet_t *packet, uint32_t *peer_id) {
   assert(packet->type == CDT_PACKET_EXISTING_PEER);
 
-  *peer_id = ntohl(*(int*)packet->data);
+  *peer_id = ntohl(*(uint32_t*)packet->data);
 
   return 0;
 }
@@ -121,4 +122,124 @@ int cdt_packet_alloc_resp_parse(cdt_packet_t *packet, uint64_t *page) {
   *page = ntohll(*(uint64_t*)packet->data);
 
   return 0;
+
+void cdt_packet_read_req_create(cdt_packet_t *packet, uint64_t page_addr) {
+  packet->type = CDT_PACKET_READ_REQ;
+  packet->size = sizeof(uint64_t);
+
+  *(uint64_t*)packet->data = htonll(page_addr);
+}
+
+void cdt_packet_read_req_parse(cdt_packet_t *packet, uint64_t *page_addr) {
+  assert(packet->type == CDT_PACKET_READ_REQ);
+
+  *page_addr = ntohll(*(uint64_t*)packet->data);
+}
+
+void cdt_packet_read_resp_create(cdt_packet_t *packet, void *page) {
+  packet->type = CDT_PACKET_READ_RESP;
+  packet->size = PGSIZE;
+
+  memmove(packet->data, page, PGSIZE);
+}
+
+void cdt_packet_read_resp_parse(cdt_packet_t *packet, void **page) {
+  assert(packet->type == CDT_PACKET_READ_RESP);
+
+  *page = packet->data;
+}
+
+void cdt_packet_read_invalidate_req_create(cdt_packet_t *packet, uint64_t page_addr) {
+  packet->type = CDT_PACKET_READ_INVALIDATE_REQ;
+  packet->size = sizeof(uint64_t);
+
+  *(uint64_t*)packet->data = htonll(page_addr);
+}
+
+void cdt_packet_read_invalidate_req_parse(cdt_packet_t *packet, uint64_t *page_addr) {
+  assert(packet->type == CDT_PACKET_READ_INVALIDATE_REQ);
+
+  *page_addr = ntohll(*(uint64_t*)packet->data);
+}
+
+void cdt_packet_read_invalidate_resp_create(cdt_packet_t *packet) {
+  packet->type = CDT_PACKET_READ_INVALIDATE_RESP;
+}
+
+void cdt_packet_write_req_create(cdt_packet_t *packet, uint64_t page_addr) {
+  packet->type = CDT_PACKET_WRITE_REQ;
+  packet->size = sizeof(uint64_t);
+
+  *(uint64_t*)packet->data = htonll(page_addr);
+}
+
+void cdt_packet_write_req_parse(cdt_packet_t *packet, uint64_t *page_addr) {
+  assert(packet->type == CDT_PACKET_WRITE_REQ);
+
+  *page_addr = ntohll(*(uint64_t*)packet->data);
+}
+
+void cdt_packet_write_resp_create(cdt_packet_t *packet, void *page) {
+  packet->type = CDT_PACKET_WRITE_RESP;
+  packet->size = PGSIZE;
+
+  memmove(packet->data, page, PGSIZE);
+}
+
+void cdt_packet_write_resp_parse(cdt_packet_t *packet, void **page) {
+  assert(packet->type == CDT_PACKET_WRITE_RESP);
+
+  *page = packet->data;
+}
+
+void cdt_packet_write_demote_req_create(cdt_packet_t *packet, uint64_t page_addr) {
+  packet->type = CDT_PACKET_WRITE_DEMOTE_REQ;
+  packet->size = sizeof(uint64_t);
+
+  *(uint64_t*)packet->data = htonll(page_addr);
+}
+
+void cdt_packet_write_demote_req_parse(cdt_packet_t *packet, uint64_t *page_addr) {
+  assert(packet->type == CDT_PACKET_WRITE_DEMOTE_REQ);
+
+  *page_addr = ntohll(*(uint64_t*)packet->data);
+}
+
+void cdt_packet_write_demote_resp_create(cdt_packet_t *packet, void *page) {
+  packet->type = CDT_PACKET_WRITE_DEMOTE_RESP;
+  packet->size = PGSIZE;
+
+  memmove(packet->data, page, PGSIZE);
+}
+
+void cdt_packet_write_demote_resp_parse(cdt_packet_t *packet, void **page) {
+  assert(packet->type == CDT_PACKET_WRITE_DEMOTE_RESP);
+
+  *page = packet->data;
+}
+
+void cdt_packet_write_invalidate_req_create(cdt_packet_t *packet, uint64_t page_addr) {
+  packet->type = CDT_PACKET_WRITE_INVALIDATE_REQ;
+  packet->size = sizeof(uint64_t);
+
+  *(uint64_t*)packet->data = htonll(page_addr);
+}
+
+void cdt_packet_write_invalidate_req_parse(cdt_packet_t *packet, uint64_t *page_addr) {
+  assert(packet->type == CDT_PACKET_WRITE_INVALIDATE_REQ);
+
+  *page_addr = ntohll(*(uint64_t*)packet->data);
+}
+
+void cdt_packet_write_invalidate_resp_create(cdt_packet_t *packet, void *page) {
+  packet->type = CDT_PACKET_WRITE_INVALIDATE_RESP;
+  packet->size = PGSIZE;
+
+  memmove(packet->data, page, PGSIZE);
+}
+
+void cdt_packet_write_invalidate_resp_parse(cdt_packet_t *packet, void **page) {
+  assert(packet->type == CDT_PACKET_WRITE_INVALIDATE_RESP);
+
+  *page = packet->data;
 }
