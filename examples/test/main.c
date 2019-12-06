50d616ea045cfb4d9475a8b8cea8c0deb205e498
Zoe Gong <zoegong@mit.edu>
2019-12-06 09:17:22 -0500

    Implementation for dot prod example - not working because of buffer overflows somewhere

--- examples/test/main.c
+++ examples/test/main.c
@@ -187,8 +187,80 @@ void* print_message(void* arg) {
   return (void*)17;
 }
 
-int main(int argc, char *argv[]) {
+typedef struct worker_assignment {
+  void * vec_a_start;
+  void * vec_b_start;
+  int section_len; 
+} worker_assignment;
+
+void* dot_product_worker(void* arg) {
+  printf("Hello from the second thread, my arg is %ld\n", (intptr_t)arg);
+  // Copy out the assignment
+  worker_assignment assignment;
+  cdt_memcpy((void *)&assignment, arg, sizeof(assignment));
+  printf("Vec a start: %p, vec b start: %p, section len: %d\n", assignment.vec_a_start, assignment.vec_b_start, assignment.section_len);
+
+  // Iterate over each byte of both vec_a and vec_b for the section, multiplying
+  intptr_t result = 0;
+  uint8_t buf_a[1];
+  uint8_t buf_b[1];
+  for (int i = 0; i < assignment.section_len; i++) {
+    cdt_memcpy(buf_a, (void *)((uint64_t)assignment.vec_a_start + i), 1);
+    cdt_memcpy(buf_b, (void *)((uint64_t)assignment.vec_b_start + i), 1);
+    result += buf_a[0] * buf_b[0];
+  }
+  return (void *)result;
+}
+
+void fill_random_vector(uint8_t * vector, int vector_size) {
+  for (int i = 0; i < vector_size; i++) {
+    *(vector + i) = random();
+  }
+}
+
+// Vector size is the length in bytes for each vector
+// Assume it is divisible by the number of cores.
+void dot_product_test(int vector_size, int num_peers) {
   cdt_init();
+  printf("Starting dot product test\n");
+  void * vec_a = cdt_malloc(vector_size);
+  void * vec_b = cdt_malloc(vector_size);
+
+  printf("malloced shared vectors: %p, %p\n", vec_a, vec_b);
+  uint8_t local_vec_a[vector_size];
+  uint8_t local_vec_b[vector_size];
+  fill_random_vector(local_vec_a, vector_size);
+  fill_random_vector(local_vec_b, vector_size);
+
+  cdt_memcpy(vec_a, (void *)local_vec_a, vector_size);
+  cdt_memcpy(vec_b, (void *)local_vec_b, vector_size);
+
+  cdt_thread_t threads[num_peers];
+  int section_size = vector_size / (cdt_get_cores() - 1);
+  printf("num_cores %d, sec size %d\n", cdt_get_cores(), section_size);
+  for (intptr_t i = 0; i < num_peers; i++) {
+    void * assignment = cdt_malloc(1);
+
+    worker_assignment local_assignment = {
+      .vec_a_start = vec_a + section_size * i,
+      .vec_b_start = vec_b + section_size * i,
+      .section_len = section_size,
+    };
+    cdt_memcpy(assignment, (void *)&local_assignment, sizeof(local_assignment));
+    cdt_thread_create(&threads[i], dot_product_worker, (void *)assignment);
+  }
+  sleep(5);
+  // int result;
+  // for (int i = 0; i < num_machines; i++) {
+  //   void *return_value;
+  //   cdt_thread_join(&threads[i], &return_value);
+  //   result += (int)return_value;
+  // }
+  // printf("Dot product example result: %d", result);
+}
+
+int main(int argc, char *argv[]) {
+  // cdt_init();
 
   // printf("Hello from the main thread\n");
   
@@ -199,6 +271,6 @@ int main(int argc, char *argv[]) {
   // cdt_thread_join(&thread, &return_value);
 
   // printf("Got %ld from other thread, done!\n", (long)return_value);
-  test2_malloc_on_mngr_write_on_peer();
+  dot_product_test(4096 * 2, 2);
   return 0;
 }
